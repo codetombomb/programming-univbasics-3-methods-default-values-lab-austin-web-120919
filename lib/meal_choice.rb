8956824ef4abf57b2e0f1ce51a65b89dfc6ebfd1
Tomas Tobar <codetombomb@gmail.com>
2019-11-25 21:40:39 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,8 +1,8 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
 
-order_to_console = "A plate of #{protien} with #{veg1} and #{veg2}."
 def meal_choice(veg1, veg2, protien="meat")
+  order_to_console = "A plate of #{protien} with #{veg1} and #{veg2}"
  puts 'What a nutritious meal!' 
  puts order_to_console
  return order_to_console
